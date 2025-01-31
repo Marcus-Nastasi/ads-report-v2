43b7d2d8e42776820bc13b935fd2c64691a892fa
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-01-31 13:08:18 -0300

    fix: debugging docker compose.

--- back-end/report/src/main/java/com/ads/report/infrastructure/configuration/redis/RedisConfiguration.java
+++ back-end/report/src/main/java/com/ads/report/infrastructure/configuration/redis/RedisConfiguration.java
@@ -4,9 +4,12 @@ import com.ads.report.infrastructure.entity.OAuth2AuthorizedClientDto;
 import com.ads.report.infrastructure.gateway.redis.RedisOAuth2AuthorizedClient;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
+import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -24,6 +27,11 @@ import org.springframework.security.oauth2.client.registration.ClientRegistratio
  * */
 @Configuration
 public class RedisConfiguration {
+
+    @Value("${spring.redis.host}")
+    private String hostName;
+    @Value("${spring.redis.port}")
+    private int port;
 
     /**
      *
@@ -49,6 +57,14 @@ public class RedisConfiguration {
         template.setKeySerializer(new StringRedisSerializer());
         template.setValueSerializer(serializer);
         return template;
+    }
+
+    @Bean
+    public LettuceConnectionFactory lettuceConnectionFactory() {
+        RedisStandaloneConfiguration redisStandaloneConfiguration = new RedisStandaloneConfiguration();
+        redisStandaloneConfiguration.setHostName(hostName);
+        redisStandaloneConfiguration.setPort(port);
+        return new LettuceConnectionFactory(redisStandaloneConfiguration);
     }
 
     @Bean
