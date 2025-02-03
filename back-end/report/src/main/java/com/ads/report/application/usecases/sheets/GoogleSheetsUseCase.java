8ec22dfe8d491c3ac5c1ae761ad34ab2d8b7fad9
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-02-03 19:00:47 -0300

    feat: Refactoring exceptions on GoogleSheetsUseCase.

--- back-end/report/src/main/java/com/ads/report/application/usecases/sheets/GoogleSheetsUseCase.java
+++ back-end/report/src/main/java/com/ads/report/application/usecases/sheets/GoogleSheetsUseCase.java
@@ -1,5 +1,6 @@
 package com.ads.report.application.usecases.sheets;
 
+import com.ads.report.application.exception.GoogleAdsException;
 import com.ads.report.application.gateway.sheets.GoogleSheetsGateway;
 import com.ads.report.domain.account.AccountMetrics;
 import com.ads.report.domain.campaign.CampaignKeywordMetrics;
@@ -7,10 +8,10 @@ import com.ads.report.domain.campaign.CampaignMetrics;
 import com.ads.report.domain.campaign.CampaignTitleAndDescription;
 import com.ads.report.domain.campaign.CampaignPerDay;
 
-import java.io.IOException;
 import java.util.List;
 
 /**
+ *
  * The use cases of google sheets.
  *
  * <p>
@@ -26,6 +27,7 @@ public class GoogleSheetsUseCase {
     private final GoogleSheetsGateway googleSheetsGateway;
 
     /**
+     *
      * Constructor of google sheets use case class.
      *
      * @param googleSheetsGateway the interface contract of google sheets available methods.
@@ -35,6 +37,7 @@ public class GoogleSheetsUseCase {
     }
 
     /**
+     *
      * This method allows the user to send account metrics directly from google ads to google sheets.
      *
      * <p>
@@ -46,13 +49,17 @@ public class GoogleSheetsUseCase {
      * @param spreadsheetId The google sheets id.
      * @param tab The sheets tab to write.
      * @param accountMetrics A list of AccountMetrics object.
-     * @throws IOException Throws exception if fails.
+     * @throws GoogleAdsException Throws exception if fails.
      */
-    public void accountMetricsToSheets(String spreadsheetId, String tab, List<AccountMetrics> accountMetrics) throws IOException {
+    public void accountMetricsToSheets(
+            String spreadsheetId,
+            String tab,
+            List<AccountMetrics> accountMetrics) throws GoogleAdsException {
         googleSheetsGateway.accountMetricsToSheets(spreadsheetId, tab, accountMetrics);
     }
 
     /**
+     *
      * This method allows the user to send campaign metrics directly from google ads to google sheets.
      *
      * <p>
@@ -64,12 +71,17 @@ public class GoogleSheetsUseCase {
      * @param spreadsheetId The google sheets id.
      * @param tab The sheets tab to write.
      * @param campaignMetrics A list of AccountMetrics object.
+     * @throws GoogleAdsException Throws exception if fails.
      */
-    public void campaignMetricsToSheets(String spreadsheetId, String tab, List<CampaignMetrics> campaignMetrics) throws IOException {
+    public void campaignMetricsToSheets(
+            String spreadsheetId,
+            String tab,
+            List<CampaignMetrics> campaignMetrics) throws GoogleAdsException {
         googleSheetsGateway.campaignMetricsToSheets(spreadsheetId, tab, campaignMetrics);
     }
 
     /**
+     *
      * This method allows the user to send client account metrics, separated per days,
      * directly from google ads to google sheets.
      *
@@ -82,21 +94,28 @@ public class GoogleSheetsUseCase {
      * @param spreadsheetId The google sheets id.
      * @param tab The sheets tab to write.
      * @param campaignPerDays the list of TotalPerDay objects.
-     * @throws IOException throws IOException if fails.
+     * @throws GoogleAdsException throws IOException if fails.
      */
-    public void totalPerDaysToSheet(String spreadsheetId, String tab, List<CampaignPerDay> campaignPerDays) throws IOException {
+    public void totalPerDaysToSheet(
+            String spreadsheetId,
+            String tab,
+            List<CampaignPerDay> campaignPerDays) throws GoogleAdsException {
         googleSheetsGateway.totalPerDayToSheets(spreadsheetId, tab, campaignPerDays);
     }
 
     /**
+     *
      * This method allows the user to send keyword metrics to sheets.
      *
      * @param spreadsheetId The google sheets id.
      * @param tab The sheets tab to write.
      * @param campaignKeywordMetrics the list of TotalPerDay objects.
-     * @throws IOException throws IOException if fails.
+     * @throws GoogleAdsException throws IOException if fails.
      */
-    public void sendKeywordMetrics(String spreadsheetId, String tab, List<CampaignKeywordMetrics> campaignKeywordMetrics) throws IOException {
+    public void sendKeywordMetrics(
+            String spreadsheetId,
+            String tab,
+            List<CampaignKeywordMetrics> campaignKeywordMetrics) throws GoogleAdsException {
 //        final double[] conversions = { 0.0 };
 //        final double[] clicks = { 0.0 };
 //        keywordMetrics.forEach(k -> k.setConversionRate(k.getConversions() / k.getClicks() * 100));
@@ -104,14 +123,18 @@ public class GoogleSheetsUseCase {
     }
 
     /**
+     *
      * This method allows the user to send titles and descriptions to sheets.
      *
      * @param spreadsheetId The google sheets id.
      * @param tab The sheets tab to write.
      * @param campaignTitleAndDescriptions the list of AdTitleAndDescriptionInfo objects.
-     * @throws IOException throws IOException if fails.
+     * @throws GoogleAdsException throws IOException if fails.
      */
-    public void sendAdTitleAndDescription(String spreadsheetId, String tab, List<CampaignTitleAndDescription> campaignTitleAndDescriptions) throws IOException {
+    public void sendAdTitleAndDescription(
+            String spreadsheetId,
+            String tab,
+            List<CampaignTitleAndDescription> campaignTitleAndDescriptions) throws GoogleAdsException {
         googleSheetsGateway.sendAdTitleAndDescription(spreadsheetId, tab, campaignTitleAndDescriptions);
     }
 }
