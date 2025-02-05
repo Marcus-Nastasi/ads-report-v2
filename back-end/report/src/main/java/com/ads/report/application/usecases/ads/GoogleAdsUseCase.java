9678d80620b0d5c079ab4b220cbdc36605a11e54
Marcus-Nastasi <vinnie.nsts@gmail.com>
2025-02-05 00:29:19 -0300

    feat: refactoring function order and comments on GoogleAdsUseCase.

--- back-end/report/src/main/java/com/ads/report/application/usecases/ads/GoogleAdsUseCase.java
+++ back-end/report/src/main/java/com/ads/report/application/usecases/ads/GoogleAdsUseCase.java
@@ -35,42 +35,6 @@ public class GoogleAdsUseCase {
 
     /**
      *
-     * Get campaigns and it's metrics.
-     *
-     * @param customerId The id of an adwords customer (client).
-     *
-     * @return The status and a list of CampaignMetrics objects.
-     * @throws RuntimeException If fails to request the data.
-     */
-    public List<CampaignMetrics> getCampaignMetrics(String customerId, String startDate, String endDate, boolean active) {
-        List<CampaignMetrics> campaignMetrics = googleAdsGateway.getCampaignMetrics(customerId, startDate, endDate, active);
-        List<LocalDate> allDates = getPeriodDaysList(startDate, endDate);
-        // Creating a map to group metrics of a single date.
-        Map<LocalDate, List<CampaignMetrics>> metricsMap = new HashMap<>();
-        for (CampaignMetrics m: campaignMetrics) {
-            LocalDate date = LocalDate.parse(m.getDate());
-            if (!metricsMap.containsKey(date)) {
-                metricsMap.put(date, new ArrayList<>());
-            }
-            metricsMap.get(date).add(m);
-        }
-        // Creating a list to have the complete results.
-        List<CampaignMetrics> completeResults = new ArrayList<>();
-        for (LocalDate date: allDates) {
-            List<CampaignMetrics> dailyMetrics = metricsMap.get(date);
-            if (dailyMetrics == null || dailyMetrics.isEmpty()) {
-                completeResults.add(new CampaignMetrics(
-                    date.toString(), date.getDayOfWeek().name(), 0L, "null", "null", "null", 0L, 0L, 0d, 0d, 0d, 0d, 0d
-                ));
-            } else {
-                completeResults.addAll(dailyMetrics);
-            }
-        }
-        return completeResults;
-    }
-
-    /**
-     *
      * Test the connection with the adwords client.
      *
      * @return The status and a list of accessible customer accounts.
@@ -110,14 +74,73 @@ public class GoogleAdsUseCase {
 
     /**
      *
-     * This method allows the user to send client account metrics, separated per days,
-     * directly from google ads to google sheets.
+     * This auxiliar method provides a list of all days of a given period.
      *
-     * <p>
-     * Here the user can pass a adwords customer id, a start date, end date,
-     * a spreadsheet id and tab, to send metrics per day directly without needing
-     * to download a csv.
-     * <p/>
+     * @param startDate The start date of the analysis period.
+     * @param endDate The end date of the analysis period.
+     *
+     * @return A list of all dates of a certain given period.
+     */
+    private List<LocalDate> getPeriodDaysList(String startDate, String endDate) {
+        // Creating a list with all dates from the given period.
+        List<LocalDate> allDates = new ArrayList<>();
+        // Parsing the date strings to LocalDate.
+        LocalDate start = LocalDate.parse(startDate);
+        LocalDate end = LocalDate.parse(endDate);
+        // Check if the start date is after the end date.
+        if (start.isAfter(end)) {
+            throw new IllegalArgumentException("The start date is after the end date");
+        }
+        // Loop into the dates to complement the dates list.
+        while (!start.isAfter(end)) {
+            allDates.add(start);
+            start = start.plusDays(1);
+        }
+        return allDates;
+    }
+
+    /**
+     *
+     * This method allows to get campaigns metrics.
+     *
+     * @param customerId The id of an adwords customer (client).
+     *
+     * @return The status and a list of CampaignMetrics objects.
+     * @throws RuntimeException If fails to request the data.
+     */
+    public List<CampaignMetrics> getCampaignMetrics(String customerId, String startDate, String endDate, boolean active) {
+        // Getting campaign metrics from Google Ads.
+        List<CampaignMetrics> campaignMetrics = googleAdsGateway.getCampaignMetrics(customerId, startDate, endDate, active);
+        List<LocalDate> allDates = getPeriodDaysList(startDate, endDate);
+        // Creating a map to group metrics of a single date.
+        Map<LocalDate, List<CampaignMetrics>> metricsMap = new HashMap<>();
+        for (CampaignMetrics m: campaignMetrics) {
+            LocalDate date = LocalDate.parse(m.getDate());
+            if (!metricsMap.containsKey(date)) {
+                metricsMap.put(date, new ArrayList<>());
+            }
+            metricsMap.get(date).add(m);
+        }
+        // Creating a list to have the complete results.
+        List<CampaignMetrics> completeResults = new ArrayList<>();
+        for (LocalDate date: allDates) {
+            List<CampaignMetrics> dailyMetrics = metricsMap.get(date);
+            if (dailyMetrics == null || dailyMetrics.isEmpty()) {
+                completeResults.add(new CampaignMetrics(
+                    date.toString(), date.getDayOfWeek().name(), 0L, "null", "null", "null", 0L, 0L, 0d, 0d, 0d, 0d, 0d
+                ));
+            } else {
+                completeResults.addAll(dailyMetrics);
+            }
+        }
+        return completeResults;
+    }
+
+    /**
+     *
+     * This method allows the user to get client account metrics separated by days.
+     *
+     * <p>Here the user can pass an adwords customer id, a start date, end date.<p/>
      *
      * @param customerId The id of an adwords customer (client).
      * @param startDate The start date of the analysis period.
@@ -126,8 +149,8 @@ public class GoogleAdsUseCase {
      * @return Returns a list of TotalPerDay object.
      */
     public List<CampaignPerDay> getTotalPerDay(String customerId, String startDate, String endDate) {
+        // Getting metrics per days.
         List<CampaignPerDay> campaignPerDays = googleAdsGateway.getTotalPerDay(customerId, startDate, endDate);
-        // Creating a list with all dates from the given period.
         List<LocalDate> allDates = getPeriodDaysList(startDate, endDate);
         // Creating a map to group metrics of a single date.
         Map<LocalDate, List<CampaignPerDay>> metricsMap = new HashMap<>();
@@ -153,7 +176,7 @@ public class GoogleAdsUseCase {
 
     /**
      *
-     * Use of getting all keyword metrics from an account.
+     * This method allows to get keyword metrics from an account.
      *
      * @param customerId The id of an adwords customer (client).
      * @param startDate The start date of the analysis period.
@@ -162,6 +185,7 @@ public class GoogleAdsUseCase {
      * @return A list of KeywordMetrics object.
      */
     public List<CampaignKeywordMetrics> getKeywordMetrics(String customerId, String startDate, String endDate, boolean active) {
+        // Getting keyword metrics form Google Ads.
         List<CampaignKeywordMetrics> campaignKeywordMetrics = googleAdsGateway.getKeywordMetrics(customerId, startDate, endDate, active);
         // Creating a list with all dates from the given period.
         List<LocalDate> allDates = getPeriodDaysList(startDate, endDate);
@@ -200,6 +224,7 @@ public class GoogleAdsUseCase {
      * @return A list of AdTitleAndDescriptionInfo object.
      */
     public List<CampaignTitleAndDescription> getAdTitleAndDescriptions(String customerId, String startDate, String endDate) {
+        // Getting title and description metrics from Google Ads.
         List<CampaignTitleAndDescription> campaignTitleAndDescriptions = googleAdsGateway.getAdTitleAndDescriptions(customerId, startDate, endDate);
         // Creating a list with all dates from the given period.
         List<LocalDate> allDates = getPeriodDaysList(startDate, endDate);
@@ -223,19 +248,5 @@ public class GoogleAdsUseCase {
             }
         }
         return completeResults;
-    }
-
-    private List<LocalDate> getPeriodDaysList(String startDate, String endDate) {
-        List<LocalDate> allDates = new ArrayList<>();
-        LocalDate start = LocalDate.parse(startDate);
-        LocalDate end = LocalDate.parse(endDate);
-        if (start.isAfter(end)) {
-            throw new IllegalArgumentException("The start date is after the end date");
-        }
-        while (!start.isAfter(end)) {
-            allDates.add(start);
-            start = start.plusDays(1);
-        }
-        return allDates;
     }
 }
